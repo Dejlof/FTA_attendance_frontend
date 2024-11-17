42177ea6f581bb2cb6fe3426573f6b15be72e660
Amos Adebisi <adebisiayodeji20@gmail.com>
2024-11-17 19:23:44 +0100

    add-package

--- src/components/SideBarButton.jsx
+++ src/components/SideBarButton.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 
-<<<<<<<<< Temporary merge branch 1
-const SideBarButton = ({ word, icon }) => {
+const SideBarButton = ({ word, icon, isSideNav }) => {
    return (
       <li>
          <div className="flex items-center pb-4 position-relative text-[8A8A8A]">
-            <div className="p-2 bg-gray-100 rounded-md">{icon}</div>
-            <span className="ml-2">{word}</span>
+            <div className="p-2 bg-gray-100 rounded-md text-base md:text-sm" title={word}>{icon}</div>
+            {isSideNav && <span className="ml-2 hidden md:inline-block">{word}</span>}
          </div>
       </li>
    );
 };
-=========
-const SideBarButton = ({word, icon}) => {
-  return (
-    <li>
-         <a href="/Overview" className="flex items-center pb-4 position-relative text-[8A8A8A]">
-          <div className='p-2 bg-gray-100 rounded-md text-base md:text-sm'>{icon}</div>
-                <span className="ml-2 hidden md:inline-block">{word}</span>
-            </a>
-    </li>
-   
-  )
-}
->>>>>>>>> Temporary merge branch 2
 
-export default SideBarButton
+export default SideBarButton;
